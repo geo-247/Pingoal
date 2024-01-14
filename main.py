3d7fe2959c9e13f354b18d57d821e7de16e91114
geo-247 <geo.official247@gmail.com>
2024-01-14 09:02:22 +0530

    update v1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 pygame.init()
 
 #initalizing screen and setting icon
@@ -6,18 +7,39 @@ screen_width, screen_height = 480, 360
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Pingoal")
 pygame.display.set_icon(pygame.image.load("assets/icon_.png"))
-running=True
 
-#iniatilizing images, assets
-bg = pygame.image.load("assets/menu.png")
-bg = pygame.transform.scale(bg, (screen_width+260,180+ screen_height))
-while running:
-  
-     #checking for events
-     screen.blit(bg,(-130,-80))
-     for event in pygame.event.get():
-          if event.type == pygame.QUIT: #cross button event function
-               running=False
-     pygame.display.update()
+def play_window():
+     while True:
+          screen.fill("BLACK")
+          bg = pygame.image.load("assets/menu.png")
+          bg = pygame.transform.scale(bg, (screen_width+260,180+ screen_height))
+          screen.blit(bg,(-130,-80))
 
-pygame.quit()
+          for event in pygame.event.get():
+               if event.type == pygame.QUIT: #cross button event function
+                    running=False
+                    pygame.quit()
+                    sys.exit()
+
+
+def main_menu():
+     running=True
+     while running:
+     #iniatilizing images, assets
+          bg = pygame.image.load("assets/menu.png")
+          bg = pygame.transform.scale(bg, (screen_width+260,180+ screen_height))
+
+          #checking for events
+          screen.blit(bg,(-130,-80))
+          for event in pygame.event.get():
+               if event.type == pygame.QUIT: #cross button event function
+                    running=False
+                    pygame.quit()
+                    sys.exit()
+                    
+          
+
+          pygame.display.update()
+          
+main_menu()
+

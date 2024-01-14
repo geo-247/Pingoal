85ed5e09ae5b323852f9bfcd7ebe3180f01816c1
geo-247 <geo.official247@gmail.com>
2024-01-14 10:03:57 +0530

    Update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import math
 from button import Button  
 
 pygame.init()
@@ -9,8 +10,38 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pongoal")
 pygame.display.set_icon(pygame.image.load("assets/icon_.png"))
 
+def is_collision(x1, y1, x2, y2):
+    distance = math.sqrt(math.pow(x1 - x2, 2) + (math.pow(y1 - y2, 2))) #applying distance formula
+    if distance < 27:
+        return True
+    else:
+        return False
+def player(x,y,playerImg):
+    resized_playerImg = pygame.transform.scale(playerImg, (30, 90))
+    screen.blit(resized_playerImg,(x,y))
+def drawCs(x,y,cs):
+    resized_playerImg = pygame.transform.scale(cs, (30, 90))
+    screen.blit(resized_playerImg,(x,y))
+
+
 def play_window():
+    #player
+    playerImg = pygame.image.load("assets/player.png")
+    playerX,playerY = 70,100
+    #cs
+    csImg = pygame.image.load("assets/enemy.png")
+    csX,csY = 390,100
+    
     while True:
+        mouseX,mouseY = pygame.mouse.get_pos()
+        playerY = mouseY #playerY movement is done with the mouse
+
+        if playerY <= 30: #limiting Y axis movement
+            playerY = 30
+        elif playerY >= 250:
+            playerY = 250
+
+
         screen.fill("BLACK")
         bg = pygame.image.load("assets/bg.png")
         bg = pygame.transform.scale(bg, (screen_width + 260, 180 + screen_height))
@@ -21,6 +52,8 @@ def play_window():
                 pygame.quit()
                 sys.exit()
 
+        player(playerX,playerY,playerImg)#drawing player
+        drawCs(csX,csY,csImg)#drawing enemy
         pygame.display.update()
 
 def main_menu():
